523813b9ee186c81edb5ddbaec9cd495dab349bf
Sungjun Choi <sungjun0110@gmail.com>
2022-03-03 17:02:05 -0800

    adds more fetch functions

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,30 @@
-const getForm = document.getElementById('getForm');
+const getFormByTeamId = document.getElementById('getFormByTeamId');
+const getFormByCompany = document.getElementById('getFormByCompany');
+const getFormByTitle = document.getElementById('getFormByTitle');
 
-getForm.addEventListener('submit', (e) => testFunction(e));
+getFormByTeamId.addEventListener('submit', (e) => getByTeamId(e));
+getFormByCompany.addEventListener('submit', (e) => getByCompany(e));
+getFormByTitle.addEventListener('submit', (e) => getByTitle(e));
 
 const BASE_URL = "http://localhost:3000/app";
 
-function testFunction(e) {
+function getByTeamId(e) {
     e.preventDefault();
-    const formData = new FormData(getForm);
-    const attendeeId = formData.get('attendeeId');
-    fetch("http://localhost:3000/app" + `/team/${attendeeId}`).then(res => res.json()).then(data => console.log(data));
+    const formData = new FormData(getFormByTeamId);
+    const teamId = formData.get('teamId');
+    fetch(BASE_URL + `/team/${teamId}`).then(res => res.json()).then(data => console.log(data));
+}
+
+function getByCompany(e) {
+    e.preventDefault();
+    const formData = new FormData(getFormByCompany);
+    const company = formData.get('company');
+    fetch(BASE_URL + `/company/${company}`).then(res => res.json()).then(data => console.log(data));
+}
+
+function getByTitle(e) {
+    e.preventDefault();
+    const formData = new FormData(getFormByTitle);
+    const title = formData.get('title');
+    fetch(BASE_URL + `/title/${title}`).then(res => res.json()).then(data => console.log(data));
 }
